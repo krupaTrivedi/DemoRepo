681e179bdb156b91daa4a17bf11388d84add4a85
Krupa Trivedi <krupa.trivedi01@gmail.com>
2021-09-16 11:37:04 -0400

    Adding Other Files

---
+++ src/main/java/tests/loginPage.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class loginPage {
+
+	
+	public void login() {
+		System.out.println("Login Test");
+	}
+}
---
+++ src/main/java/tests/searchPage.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class searchPage {
+  public void search() {
+	  System.out.println("Search Page");
+  }
+}
